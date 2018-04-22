7612f60c2d8da6ea9c24a6f1c7604bb3d2b1567c
Craig Butler <craigwb@gmx.com>
2018-04-22 19:20:41 -0400

    Add files via upload

--- NameServer.java
+++ NameServer.java
@@ -9,50 +9,55 @@ import java.util.Scanner;
 
 public class NameServer extends Thread {
 	Node pairs[];
-	NameServer pred, succ;
-	int port;
+	int pred, succ;
+	int port, ID;
+	int IDs[] = new int[1024];
+	int i = 0;
+	int count = 0;
+	Socket servers[] = new Socket[1024];
 	Socket bootstrap;
 	BufferedReader config;
-	String inPair;
-	String userIn;
-	Scanner split;
-	Scanner keyIn;
-	int ID;
-	// int port;
-	boolean quit = false;
-	private volatile boolean exit = false;
-
+	String inPair, userIn;
+	Scanner split, keyIn;
+	volatile boolean quit = false;
+	volatile boolean exit;
 	public static void main(String args[]) {
 		NameServer ns = new NameServer(args);
 	}
 
 	NameServer(String args[]) {
-		try {
-			// initialize config
+		try { // initialize config
 			config = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
 			ID = Integer.parseInt(config.readLine());
 			port = Integer.parseInt(config.readLine());
 			if (ID > 0) { // not bootstrap
 				System.out.println("Name Server");
-				split = new Scanner(config.readLine());
-				bootstrap = new Socket(split.next(), split.nextInt());
 				keyIn = new Scanner(System.in);
+				split = new Scanner(config.readLine());
 				while (!quit) {
-					userIn = "";
-					if (keyIn.hasNext()) {
-						split = new Scanner(keyIn.nextLine());
-						if (split.hasNext()) {
-							userIn = split.next();
-							if (userIn.equalsIgnoreCase("enter")) {
-								start();
-								System.out.println("Name Server listening on: " + port);
-							} else if (userIn.equalsIgnoreCase("exit"))
-								quit = true;
-							else
-								System.out.println("Input not understood.");
+					userIn = keyIn.nextLine();
+					if (userIn.equalsIgnoreCase("enter")) {
+						bootstrap = new Socket(split.next(), split.nextInt());
+						PrintWriter out = new PrintWriter(bootstrap.getOutputStream(), true);
+						BufferedReader in = new BufferedReader(new InputStreamReader(bootstrap.getInputStream()));
+						out.println(ID);
+						out.flush();
+						System.out.println(ID);
+						int nodes = Integer.parseInt(in.readLine());
+						System.out.println(nodes);
+						pairs = new Node[nodes];
+						for (int i = 0; i<nodes;i++) {
+							pairs[i]= new Node(Integer.parseInt(in.readLine()),in.readLine());
+							System.out.println(pairs[i].key + " " + pairs[i].value);
 						}
-					}
-
+						System.out.println("Name Server listening on: " + port);
+					} 
+					else if (userIn.equalsIgnoreCase("exit"))
+						System.out.println();
+					else if (userIn.equalsIgnoreCase("quit"))
+						quit = true;
+					else
+						System.out.println("Input not understood.");
 				}
 			} else if (ID == 0) { // bootstrap
 				start();
@@ -95,26 +100,39 @@ public class NameServer extends Thread {
 
 	public void run() {
 		if (ID == 0) {
-			Socket servers[] = new Socket[1024];
-			int IDs[] = new int[1024];
-			int i = 0;
-			int count=0;
 			ServerSocket server;
 			Socket socket;
 			try {
 				server = new ServerSocket(port);
-				server.setSoTimeout(2000);
-				while (!exit) {
+				while (!quit) {
 					try {
 						socket = server.accept();
 						PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 						BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+						System.out.println("trying");
 						int ID = Integer.parseInt(in.readLine());
+						System.out.println(ID);
 						IDs[i] = ID;
 						servers[ID] = socket;
+						i++;
+						count++;
+						succ = 1024;
+						pred = 0;
+						for (int x = 0; x<i;x++) {
+							if ((IDs[x]!=-1) && (IDs[x]-ID)<(succ-ID) && IDs[x]-ID>0) succ = IDs[x];
+							if ((IDs[x]!=-1) && (ID-IDs[x])<(ID-pred) && ID-IDs[x]>0) pred = IDs[x];
+						}
 						
-					} catch (SocketTimeoutException e) {
-						
+						out.println(ID-pred);
+						out.flush();
+						for (int x = 0; x<ID-pred;x++) {
+							out.println(pred+x);//id
+							out.flush();
+							out.println(lookup(pred+x));//value
+							out.flush();
+						}
+					} catch (Exception e) {
+
 					}
 
 				}
@@ -122,31 +140,14 @@ public class NameServer extends Thread {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+
 		} else {
-			ServerSocket server;
-			Socket socket;
-			try {
-				server = new ServerSocket(port);
-				server.setSoTimeout(2000);
-				while (!exit) {
-					try {
-						socket = server.accept();
-					} catch (SocketTimeoutException e) {
-						
-					}
-
-				}
-				server.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
 	public void quit() {
 		System.out.println("Quitting safely, this may take a few seconds.");
-		exit = true;
+		quit = true;
 	}
 
 	public void insert(int key, String value) {
@@ -160,18 +161,23 @@ public class NameServer extends Thread {
 		// System.out.println(pairs[i].key + pairs[i].value);
 	}
 
-	public void lookup(int key) {
+	public String lookup(int key) {
 		int i;
 		boolean found = false;
 		for (i = 0; i < pairs.length; i++) {
 			if (pairs[i] != null && pairs[i].key == key) {
-				System.out.println(key + ": " + pairs[i].value);
+				System.out.println(key + " " + pairs[i].value);
 				found = true;
 				break;
 			}
 		}
-		if (!found)
-			System.out.println("Key not held");
+		if (!found) {
+			System.out.println(key+" Key not held");
+			return "Key not held";
+		}
+		else {
+			return pairs[i].value;
+		}
 	}
 
 	public void delete(int key) {
@@ -193,5 +199,6 @@ public class NameServer extends Thread {
 			this.value = value;
 		}
 	}
+	
 
 }
